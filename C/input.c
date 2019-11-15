d139f33b9ee8aa8c74b80ba81a7f9d03f1eab283
dambergn <dambergn@gmail.com>
2019-11-15 15:33:00 -0800

    ACP"

---
+++ C/input.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int radius;
+    printf("Please enter a number:");
+    scanf("%i", &radius);
+
+    printf("The given number is %i\n", radius);
+    return 0;
+}
