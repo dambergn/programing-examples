224f05669f2cb6748a751e9cc0a2bd94919aaab5
dambergn <dambergn@gmail.com>
2020-01-10 08:22:53 -0800

    modified bash function convention.

--- bash/function.sh
+++ bash/function.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Passing arguments into function 
-function toRun () {
+function func_toRun () {
     value1="${1}" # $1 represent first argument
     value2="${2}" # $2 represent second argument
     echo 'I have' ${value1}
     echo 'This is how to' ${value2}
 }
 
-toRun "suceeded" "pass arguments into a function"
+func_toRun "suceeded" "pass arguments into a function"
