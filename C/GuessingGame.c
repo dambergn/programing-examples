541a3c20ea466380fab35fe9ddc6981aa10942f4
nicholas.damberg <nicholas.damberg@bigfishgames.com>
2020-10-21 16:01:49 -0700

    guessing game

--- C/GuessingGame.c
+++ C/GuessingGame.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+// The user has to guess a number from 0-x
+// Output weather or not the person is correct.
 
 int main(){
+    // Generate a random number
+    // pseudo random number generator
+    // seed - from the clock
+    int maxValue = 5;
+    srand(time(NULL)); //
+    int randomNumber = rand() % maxValue + 1;
+
+    printf("%d\n", randomNumber);
+
+    // modulus operator %
+    // remainder of some division
+
+    printf("Guess a number 0 - %d: ", maxValue);
+    int input;
+    scanf("%d", &input);
+
+    if(input == randomNumber){
+        printf("You have guessed wisely\n");
+    }else{
+        printf("You have guessed poorly\n");
+    }
 
   return 0;
 }
