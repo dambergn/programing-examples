06d75f70e84164c971b5d499d472a7a9a159d237
dambergn <dambergn@gmail.com>
2020-10-29 20:35:46 -0700

    added print statements

--- raspberrypi/python/led/blink_led_RGB.py
+++ raspberrypi/python/led/blink_led_RGB.py
@@ -13,6 +13,7 @@ GPIO.setup(blue_led, GPIO.OUT)
 GPIO.setup(red_led, GPIO.OUT)
 for x in range(0, 7):
     # Green LED
+    print("Green")
     GPIO.output(green_led, True)
     GPIO.output(blue_led, False)
     GPIO.output(red_led, False)
@@ -21,18 +22,21 @@ for x in range(0, 7):
     time.sleep(.1)
 
     # Blue LED
+    print("Blue")
     GPIO.output(blue_led, True)
     time.sleep(.5)
     GPIO.output(blue_led, False)
     time.sleep(.1)
 
     # Red LED
+    print("Red")
     GPIO.output(red_led, True)
     time.sleep(.5)
     GPIO.output(red_led, False)
     time.sleep(.1)
 
     # Purple
+    print("Purple")
     GPIO.output(blue_led, True)
     GPIO.output(red_led, True)
     time.sleep(.5)
