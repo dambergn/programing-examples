ab8040ae2e82ee45f23215044f4782afd295ad77
dambergn <dambergn@gmail.com>
2020-10-30 23:27:13 -0700

    updated RGB led

--- raspberrypi/python/led/blink_led_RGB.py
+++ raspberrypi/python/led/blink_led_RGB.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
 
 green_led = 11
 blue_led = 13
@@ -11,27 +12,27 @@ red_led = 15
 GPIO.setup(green_led, GPIO.OUT)
 GPIO.setup(blue_led, GPIO.OUT)
 GPIO.setup(red_led, GPIO.OUT)
-for x in range(0, 7):
+for x in range(0, 3):
     # Green LED
     print("Green")
     GPIO.output(green_led, True)
     GPIO.output(blue_led, False)
     GPIO.output(red_led, False)
-    time.sleep(.5)
+    time.sleep(1)
     GPIO.output(green_led, False)
     time.sleep(.1)
 
     # Blue LED
     print("Blue")
     GPIO.output(blue_led, True)
-    time.sleep(.5)
+    time.sleep(1)
     GPIO.output(blue_led, False)
     time.sleep(.1)
 
     # Red LED
     print("Red")
     GPIO.output(red_led, True)
-    time.sleep(.5)
+    time.sleep(1)
     GPIO.output(red_led, False)
     time.sleep(.1)
 
@@ -39,7 +40,7 @@ for x in range(0, 7):
     print("Purple")
     GPIO.output(blue_led, True)
     GPIO.output(red_led, True)
-    time.sleep(.5)
+    time.sleep(1)
     GPIO.output(blue_led, False)
     GPIO.output(red_led, False)
     time.sleep(.1)
