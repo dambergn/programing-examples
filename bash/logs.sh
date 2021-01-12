505a6b76449cc0364ad017c24956ca1a5b34c328
dambergn <dambergn@gmail.com>
2021-01-11 18:54:58 -0800

    made logs example better

--- bash/logs.sh
+++ bash/logs.sh
@@ -2,10 +2,12 @@
 
 START=$(date +%T)
 NOW=$(date +"%Y-%m-%d-${START}")
+NAME="log"
+DIR="logs/"
 
 
 (
 echo 'this will be saved in the log'
 sleep 5
 echo 'this is another log entry'
-) 2>&1 | tee -a log-${NOW}.txt
+) 2>&1 | tee -a ${DIR}${NAME}-${NOW}.txt
